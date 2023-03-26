0bb31304790b5c195d10f7d31f754309bffb9232
Mikayel <90648291+mikayel00@users.noreply.github.com>
2023-03-26 14:45:28 +0400

    Mongo connection fix (#2)
    * fix: change mongo-db connection fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../modules/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_CONNECTION_NAME } from './constants';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,11 +12,13 @@ import { MongooseModule } from '@nestjs/mongoose';
       load: [configs],
     }),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       }),
+      inject: [ConfigService],
+      connectionName: MONGO_CONNECTION_NAME,
     }),
     TicketsModule,
     UsersModule,
